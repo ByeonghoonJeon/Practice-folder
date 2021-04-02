9db620c3a590ba38f2c5995e06c6f5d09c12633c
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-02 16:34:50 +0200

    Generating possobilities with randomisation.

--- practice_randomisation.py
+++ practice_randomisation.py
@@ -2,4 +2,12 @@
 import random
 
 random_int = random.randint(1, 100)
-print(random_int)
+
+if random_int in range(1, 11):
+    print("top 10")
+elif random_int in range(11, 31):
+    print("top 30")
+elif random_int in range(31, 71):
+    print("top 70")
+else:
+    print(random_int)
