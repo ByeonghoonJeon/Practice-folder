d17679494a999c23ef11194207798c411632bc3d
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-02 21:10:11 +0200

    Randomisation practice

--- practice_randomisation.py
+++ practice_randomisation.py
@@ -1,13 +1,52 @@
 # This programm is for practice of randomisation code.
 import random
 
-random_int = random.randint(1, 100)
+print("Welcome to lotto predictor!")
 
-if random_int in range(1, 11):
-    print("top 10")
-elif random_int in range(11, 31):
-    print("top 30")
-elif random_int in range(31, 71):
-    print("top 70")
-else:
-    print(random_int)
+# Ask number range.
+number_range = input("Please the largest number in your country's lotto system.\n")
+
+# Check if input is number.
+
+while not number_range.isdigit():
+    number_range = input("Please input number only\n")
+
+# Randomise 6 numbers.
+
+random_int1 = random.randint(1, int(number_range))
+random_int2 = random.randint(1, int(number_range))
+random_int3 = random.randint(1, int(number_range))
+random_int4 = random.randint(1, int(number_range))
+random_int5 = random.randint(1, int(number_range))
+random_int6 = random.randint(1, int(number_range))
+
+# Avoid duplicated numbers among 6 numbers above.
+
+while (
+    (random_int1 == random_int2)
+    or (random_int1 == random_int3)
+    or (random_int1 == random_int4)
+    or (random_int1 == random_int5)
+    or (random_int1 == random_int6)
+    or (random_int2 == random_int3)
+    or (random_int2 == random_int4)
+    or (random_int2 == random_int5)
+    or (random_int2 == random_int6)
+    or (random_int3 == random_int4)
+    or (random_int3 == random_int5)
+    or (random_int3 == random_int6)
+    or (random_int4 == random_int5)
+    or (random_int4 == random_int6)
+    or (random_int5 == random_int6)
+):
+    random_int1 = random.randint(1, int(number_range))
+    random_int2 = random.randint(1, int(number_range))
+    random_int3 = random.randint(1, int(number_range))
+    random_int4 = random.randint(1, int(number_range))
+    random_int5 = random.randint(1, int(number_range))
+    random_int6 = random.randint(1, int(number_range))
+
+# Print result.
+
+print(random_int1, random_int2, random_int3, random_int4, random_int5, random_int6)
+print("Good Luck!")
