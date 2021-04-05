4aa7ec85dbaf4280b87b8564cd10b2540a0796e0
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-05 23:47:19 +0200

    Adding Pseudo Codes for clearer control structure

--- cafe_order.py
+++ cafe_order.py
@@ -8,9 +8,11 @@
 #               [2-1-3-1-1] If quantity is digit,
 #                   [2-1-3-1-1-1] Accept order.
 #                   [2-1-3-1-1-2] Count bill.
-#                   [2-1-3-1-1-3] Ask size with informing extra fee for the tall size. (Regular and tall size(+ $ 0.5))
-#                       [2-1-3-1-1-3-1] If input properly, count bill.
-#                       [2-1-3-1-1-3-2] If input is invalid code, go to [2-1-3-1-1-3].
+#                   [2-1-3-1-1-3] Check if the order is a beverage,
+#                       [2-1-3-1-1-3-1] If a beverage, ask size choice with informing extra fee for the tall size. (Regular and tall size(+ $ 0.5))
+#                           [2-1-3-1-1-3-1-1] If input properly, count bill.
+#                           [2-1-3-1-1-3-1-2] If input is invalid code, go to [2-1-3-1-1-3-1].
+#                       [2-1-3-1-1-3-2] If the order is not a beverage, go to [2-1-3-1-1-4].
 #                    [2-1-3-1-1-4] Ask if a customer wants other menu as well. (Y/N)
 #                        [2-1-3-1-1-4-1] If Y, go to [2]
 #                        [2-1-3-1-1-4-2] If N, go to [Final]
@@ -26,12 +28,11 @@
 #   [2-3] If input does not exist in the menu, go to [2] requesting valid menu.
 
 # [Final] Print total bill and say goodbye.
-
+###########################################################################################################################
 
 # Variations for total bill
-
 bev_bill = 0
-exta_bill = 0
+size_bill = 0
 
 # Expected input for orders from customers.
 
