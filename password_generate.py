e36ec91e1177b3dec7a8039cc68f3ce4e474c2e3
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-06 22:07:38 +0200

    Working on making a password generator

--- password_generate.py
+++ password_generate.py
@@ -1,29 +1,98 @@
-# Before start, I practice for loop and while loop again with Udemy course.
+import random
 
-student_heights = input("Please input student heights.\n").split()
-for i in range(0, len(student_heights)):
-    student_heights[i] = int(student_heights[i])
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
 
-total_height = 0
-for height in student_heights:
-    total_height += height
-print("Total height is", total_height)
+symbols = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "=",
+    "+",
+    ">",
+    "<",
+    "?",
+    ";",
+    ":",
+    ",",
+    ".",
+    "/",
+]
 
-number_of_students = 0
-for student in student_heights:
-    number_of_students += 1
-print("Total number of students is", number_of_students)
-average_height = total_height / number_of_students
-print("The average is", round(average_height, 1))
-
-highest_value = 0
-for height in student_heights:
-    if height > highest_value:
-        highest_value = height
-print("The highest value is", highest_value)
-
-total = 0
-for number in range(1, 101):
-    total += number
-
-print(total)
+print("Welcome to password generator!")
+number_of_letters = input("How many letters would you like to have in your password?\n")
+while not number_of_letters
+number_or_numbers = 
