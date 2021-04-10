b85c1216f2e15b4b970e53171020029c49b2b50f
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-11 00:39:12 +0200

    Modifying code

--- hangman.py
+++ hangman.py
@@ -63,28 +63,57 @@ word_list = [
     "Feminism",
 ]
 
+
 random_word = random.choice(word_list)
 len_word = int(len(random_word))
-guessed_letters = set()
+guessed_letters = []
 print(" _ " * len_word)
+
+current_guess = input(f"Guess the {len_word} lettered word!\n").lower()
+
 while True:
-    guess_letter = input(f"Guess the {len_word} lettered word!\n").lower()
-
     while (
-        len(guess_letter) != 1
-        or guess_letter in list(string.punctuation)
-        or guess_letter in list(string.digits)
+        len(current_guess) != 1
+        or current_guess in list(string.punctuation)
+        or current_guess in list(string.digits)
     ):
-
-        guess_letter = input(f"Please input single Alphabet only.\n").lower()
-
-    if guess_letter in random_word.lower() and guess_letter not in guessed_letters:
-        guessed_letters.add(guess_letter)
-
-    for letter in random_word.lower():
-        if letter in guessed_letters:
-            print(f" {letter} ", end=(""))
+        current_guess = input(f"Please input single Alphabet only.\n").lower()
+    guessed_letters += current_guess
+    for guessed_letters in random_word.lower():
+        if guessed_letters in random_word.lower():
+            print(f"{guessed_letters}")
             continue
-        else:
-            print(" _ ", end=(""))
+        elif guessed_letters not in random_word.lower():
+            current_guess = input("Please try again!")
             continue
+        elif random_word.lower() in guessed_letters:
+            print("Splendid!")
+            break
+print("The end.")
+
+
+# random_word = random.choice(word_list)
+# len_word = int(len(random_word))
+# guessed_letters = set()
+# print(" _ " * len_word)
+# while True:
+#     guess_letter = input(f"Guess the {len_word} lettered word!\n").lower()
+
+#     while (
+#         len(guess_letter) != 1
+#         or guess_letter in list(string.punctuation)
+#         or guess_letter in list(string.digits)
+#     ):
+
+#         guess_letter = input(f"Please input single Alphabet only.\n").lower()
+
+#     if guess_letter in random_word.lower() and guess_letter not in guessed_letters:
+#         guessed_letters.add(guess_letter)
+
+#     for letter in random_word.lower():
+#         if letter in guessed_letters:
+#             print(f" {letter} ", end=(""))
+#             continue
+#         else:
+#             print(" _ ", end=(""))
+#             continue
