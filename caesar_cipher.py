31078d061610bb97836b1af62dd5527a91a0c349
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-13 23:04:11 +0200

    Working on

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,3 +7,24 @@
 #       Ask to input a encoded phrase.
 #       Ask to type assigned shift number.
 # Ask if a user wants to go to beginning again.
+
+print("Welcome to Jeon's encoding program.")
+while True:
+    encode_or_decode = input(
+        "Please type 'encode' to encrypt, or 'decode' to decrypt.\n"
+    )
+    # Check whether the input is valid answer.
+    expected_input = ["encode", "decode"]
+    while encode_or_decode.lower() not in expected_input:
+        encode_or_decode = input(
+            "Please type 'encode' to encrypt, or 'decode' to decrypt.\n"
+        )
+    if encode_or_decode.lower() == "encode":
+        message = input("Please type your message.\n")
+        print("Message received.")
+
+        shift_number = input("Please type the shift number\n")
+        # Check whether the input is valid number.
+
+    else:
+        message = input("Please type your encoded message.")
