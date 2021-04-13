ce8e6842b6c58be8e820d217daffd614c0e2d472
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-13 22:48:05 +0200

    Working on the new project

---
+++ caesar_cipher.py
@@ -0,0 +1,9 @@
+# Greeting
+# Ask if a user wants encode or decode.
+#   If a user wants encode,
+#       Ask to input a phrase.
+#       Ask to type the shift number.
+#   If a user wants decode,
+#       Ask to input a encoded phrase.
+#       Ask to type assigned shift number.
+# Ask if a user wants to go to beginning again.
