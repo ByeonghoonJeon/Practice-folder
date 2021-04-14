f6d3a5821897ea22a6c5606f87cbcdd80be6872b
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-14 16:36:50 +0200

    Working on

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,6 +7,13 @@
 #       Ask to input a encoded phrase.
 #       Ask to type assigned shift number.
 # Ask if a user wants to go to beginning again.
+from time import sleep
+import string
+
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+letters += numbers
+
 
 print("Welcome to Jeon's encoding program.")
 while True:
@@ -33,12 +40,14 @@ while True:
         while (shift_number) not in acceptable_number_range:
             shift_number = input("Please type the shift number. (1 ~ 100)\n")
 
-        print("Encoding...")
+        for i in range(0, 3):
+            print("Encoding." + "." * i)
+            sleep(0.3)
 
         # Encode starts from here.
 
     else:
-        message = input("Please type your encoded message.")
+        message = input("Please type your encoded message.\n")
         print("Encoded message received.")
         shift_number = input("Please type the shift number.\n")
         # Check whether the input is valid number.
@@ -50,8 +59,11 @@ while True:
         while (shift_number) not in acceptable_number_range:
             shift_number = input("Please type the shift number. (1 ~ 100)\n")
 
-        print("Decoding...")
+        for i in range(0, 3):
+            print("Decoding." + "." * i)
+            sleep(0.3)
 
+    # Check if the user wants to go again or not.
     repeat = input("Please type 'yes' if you want to go again. Otherwise, type 'no'.\n")
     while repeat.lower() not in ("yes", "no"):
         repeat = input(
