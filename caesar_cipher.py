e44f475da3ba6857771c92bbe7cd41ce9b1a6cee
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-14 17:50:31 +0200

    Working on

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -13,11 +13,8 @@ import string
 letters = list(string.ascii_letters)
 numbers = list(string.digits)
 letters += numbers
+print(letters)
 
-count = 0
-for i in letters:
-    count += 1
-print(count)
 print("Welcome to Jeon's encoding program.")
 while True:
     encode_or_decode = input(
@@ -31,34 +28,36 @@ while True:
         )
     if encode_or_decode.lower() == "encode":
         message = input("Please type your message.\n")
+        while not message.isalnum():
+            message = input("Please type letters and numbers only.")
         print("Message received.")
 
-        shift_number = input("Please type the shift number.\n")
+        shift_number = input("Please type the shift number. (1 ~ 61)\n")
         # Check whether the input is valid number.
-        acceptable_number_range = []
-        for a_number in range(1, 101):
-            acceptable_number_range.append(a_number)
+        while (
+            not shift_number.isdigit()
+            or int(shift_number) < 1
+            or int(shift_number) > 61
+        ):
+            shift_number = input("Please type the shift number. (1 ~ 61)\n")
         shift_number = int(shift_number)
-
-        acceptable_number_range = str(acceptable_number_range)
-        while str(shift_number) not in acceptable_number_range:
-            shift_number = input("Please type the shift number. (1 ~ 100)\n")
-
         for i in range(0, 3):
             print("Encoding." + "." * i)
             sleep(0.3)
 
         def encode_letter(message, shift_number):
+            encoded_message = ""
             for letter in message:
                 original_position = letters.index(letter)
 
-                if original_position + shift_number > 62:
+                if int(original_position) + int(shift_number) > 61:
                     encoded_position = original_position + (shift_number - 62)
                 else:
                     encoded_position = original_position + shift_number
 
-                encoded_letter = letters[encoded_position]
-                return print(encoded_letter)
+                encoded_letter = letters[int(encoded_position)]
+                encoded_message += encoded_letter
+            return print(f"Encoded message is: [{encoded_message}]")
 
         encode_letter(message, shift_number)
 
@@ -66,16 +65,18 @@ while True:
 
     else:
         message = input("Please type your encoded message.\n")
+        while message not in letters:
+            message = input("Please type letters and numbers only.")
         print("Encoded message received.")
         shift_number = input("Please type the shift number.\n")
         # Check whether the input is valid number.
         acceptable_number_range = []
-        for a_number in range(1, 101):
+        for a_number in range(1, 62):
             acceptable_number_range.append(a_number)
 
         acceptable_number_range = str(acceptable_number_range)
         while (shift_number) not in acceptable_number_range:
-            shift_number = input("Please type the shift number. (1 ~ 100)\n")
+            shift_number = input("Please type the shift number. (1 ~ 61)\n")
 
         for i in range(0, 3):
             print("Decoding." + "." * i)
