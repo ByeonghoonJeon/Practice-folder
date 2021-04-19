b25922ebf87b21a060f5e7b9224237eca140f18e
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-19 10:53:09 +0200

    Working on a Secret Auction Programm

---
+++ secret_auction.py
@@ -0,0 +1,7 @@
+# Greeting
+# Ask name
+# Ask bid
+# Ask if there are any other bidders
+# if there are any other bidders, go to line 2
+# if there are no other bidders, finish the auction.
+# Shou result.
