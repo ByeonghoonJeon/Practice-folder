f101c927fe0f6bc06d560e2e16720ef1426af8ae
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-19 17:05:03 +0200

    Working on

--- grading_program.py
+++ grading_program.py
@@ -5,17 +5,28 @@ students_score = {}
 
 #   2.1 Creat a function that evaluate the validity of name.
 def check_name(name):
-    #   2.1.1 Replace period and blank in the name to "" and check it if the name is comprised with only Alphabet.
-    name.replace(".", "")
-    name.replace(" ", "")
-    return name.isalpha()
+    #   2.1.1 Remove period and blank and check it if the name is comprised with only Alphabet.
+    #   2.1.1.1 Make a list of spelling in the name.
+    list_of_spelling = list(name)
+    #   2.1.1.2 Remove period and blank from the list.
+    while "." in list_of_spelling:
+        list_of_spelling.remove(".")
+    while " " in list_of_spelling:
+        list_of_spelling.remove(" ")
+
+    #   2.1.1.3 Convert the list to a string.
+    list_to_string = ""
+    list_to_string = list_to_string.join(list_of_spelling)
+
+    #   2.1.1.4 Return if the string is Alphabet.
+    return list_to_string.isalpha()
 
 
+#   2.2 Input student's name.
 name = input("Please input student's name. \n")
 check_name(name)
-while check_name(name) == False:
+#   2.3 Check if the name is alphabet. If not, ask to input correct name again.
+while check_name(name) != True:
     name = input("Please input student's name. (Alphabet and period only.)\n")
-#   2.2 Name input
 
-
-student_score = input(f"Please input {name}'s score.")
+score = input(f"Please input {name}'s score.\n")
