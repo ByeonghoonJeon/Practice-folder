60f5653b71edb697746424bdde8eb9e36bb696bb
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-19 12:39:49 +0200

    Working on

--- grading_program.py
+++ grading_program.py
@@ -1,17 +1,29 @@
 # 1. Create students score dictionary.
 students_score = {}
 
-# 2. Input students name and score, and check if input is correct. (Letters and punctuation only.)
+# 2. Input students name and score, and check if input is correct. (Alphabet, period, and blank only.)
 
-#   2.1 Name validity checking function.
+#   2.1 Creat a function that evaluate the validity of name.
 def check_name(name):
-    split_name = name.split()
-    for i in split_name:
-        if i.isalpha() == True:
-            return "valid"
+    #   2.1.1 Split all the words to evaluate if those are all acceptable characters. (Alphabet, period, and blank.)
+    split_name_into_letters = list(name)
+    #   2.1.2 Replace "." and " " to a random alphabet
+    for i in range(len(split_name_into_letters)):
+        if split_name_into_letters[i] == ".":
+            split_name_into_letters[i] = "a"
+        if split_name_into_letters[i] == " ":
+            split_name_into_letters[i] = "a"
+        #   2.1.3 If input name is comprised with Alphabet, return "valid"
+        for i in range(len(split_name_into_letters)):
+            if split_name_into_letters[i].isalpha == True:
+                return "valid"
 
 
-# 2.2 Name input
+#   2.2 Name input
 name = input("Please input student's name. \n")
+#   2.3 Evaluation of name
 while check_name(name) != "valid":
     name = input("Please input student's name. (Alphabet and period only)\n")
+    check_name(name)
+print(f"Student name: {name}")
+student_score = input("Please input student's score.")
