aa1db37163b7ce1be03c6a55a72954bf7f1274ec
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-04-19 15:40:03 +0200

    Working on

--- grading_program.py
+++ grading_program.py
@@ -5,25 +5,17 @@ students_score = {}
 
 #   2.1 Creat a function that evaluate the validity of name.
 def check_name(name):
-    #   2.1.1 Split all the words to evaluate if those are all acceptable characters. (Alphabet, period, and blank.)
-    split_name_into_letters = list(name)
-    #   2.1.2 Replace "." and " " to a random alphabet
-    for i in range(len(split_name_into_letters)):
-        if split_name_into_letters[i] == ".":
-            split_name_into_letters[i] = "a"
-        if split_name_into_letters[i] == " ":
-            split_name_into_letters[i] = "a"
-        #   2.1.3 If input name is comprised with Alphabet, return "valid"
-        for i in range(len(split_name_into_letters)):
-            if split_name_into_letters[i].isalpha == True:
-                return "valid"
+    #   2.1.1 Replace period and blank in the name to "" and check it if the name is comprised with only Alphabet.
+    name.replace(".", "")
+    name.replace(" ", "")
+    return name.isalpha()
 
 
-#   2.2 Name input
 name = input("Please input student's name. \n")
-#   2.3 Evaluation of name
-while check_name(name) != "valid":
-    name = input("Please input student's name. (Alphabet and period only)\n")
-    check_name(name)
-print(f"Student name: {name}")
-student_score = input("Please input student's score.")
+check_name(name)
+while check_name(name) == False:
+    name = input("Please input student's name. (Alphabet and period only.)\n")
+#   2.2 Name input
+
+
+student_score = input(f"Please input {name}'s score.")
