4c8a63e286a844d17d6897ba561d3fc559234e37
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-17 21:58:20 +0700

    Practice

--- javascript practice/practice.js
+++ javascript practice/practice.js
@@ -30,6 +30,22 @@ alert (message.slice(0,10));
 var userName = prompt("Please input your name");
 var firstLetter = userName.slice(0,1);
 var firstUpperLetter=firstLetter.toUpperCase();
-var elseLetters = userName.slice(1,999);
+var elseLetters = userName.slice(1,userName.length);
 var elseLowerLetters = elseLetters.toLowerCase();
 alert ("Hello, "+firstUpperLetter+elseLowerLetters);
+
+
+// Dog age calculator
+var dogYear = prompt("Please input your dot's age");
+var dogAgeConvert = (dogYear-2)*4+21;
+alert ("Your dog's age is "+dogAgeConvert + " in human age.");
+
+// Increment and Decrement
+var x = 5;
+x = x +1;
+x ++;
+x+=3 // x = x+3
+
+var y = 5;
+y = y-1
+y --;
