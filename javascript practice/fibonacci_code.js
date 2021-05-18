a30b1bffd193d8002dd163a079e96c6ef9180905
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-18 22:23:39 +0700

    Fibonacci Code Finished Version

--- javascript practice/fibonacci_code.js
+++ javascript practice/fibonacci_code.js
@@ -1,10 +1,12 @@
+// Set a function.
+function fibonacciCode(finalNumber){
 // Set initial Fibonacci order.
 var fibonacci = [0, 1]
-// Ask how many numbers does user want.
-var finalNumber = prompt("Please input the last order of Fibonacci code.");
 // Initial "n" is zero, final n is finalNumber-2, and add 1 for every loop. And add to the array.
 for (var n=0; n<finalNumber-2; n++){
     fibonacci.push(fibonacci[n]+fibonacci[n+1])}
-alert (fibonacci)
+    return (fibonacci)
+}
+fibonacciCode()
 
 
