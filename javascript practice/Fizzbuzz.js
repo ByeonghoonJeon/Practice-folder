e151809c7b61055da49ed5e479ac943463c12b8d
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-18 20:29:19 +0700

    Practice

--- javascript practice/Fizzbuzz.js
+++ javascript practice/Fizzbuzz.js
@@ -1,12 +1,13 @@
-
 var userNumber = [];
 var countNumber = 1;
 function fizzbuzz(){
 
 if (countNumber%3===0 && countNumber%5===0){userNumber.push("FizzBuzz")};
-if (countNumber%3 === 0) {userNumber.push("Fizz")};
-if (countNumber%5 === 0) {userNumber.push("Buzz")};
+if (countNumber%3 === 0 && countNumber%5 !== 0) {userNumber.push("Fizz")};
+if (countNumber%5 === 0 && countNumber%3 !==0) {userNumber.push("Buzz")};
 if (countNumber%3 !== 0 && countNumber%5 !==0){userNumber.push(countNumber)};
 countNumber++;
 alert(userNumber);
 }
+
+while (countNumber <= 100){fizzbuzz()}
