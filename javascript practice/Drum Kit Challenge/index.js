49d8d6a4f1d94a45e4040bf0bc4da1709a951339
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-19 19:56:54 +0700

    Practice

--- javascript practice/Drum Kit Challenge/index.js
+++ javascript practice/Drum Kit Challenge/index.js
@@ -1,2 +1,3 @@
-document.querySelector("button").addEventListener("click","handleClick");
+for (var n = 0; n<7; n++){
+document.querySelectorAll("button")[n].addEventListener("click",handleClick);}
 function handleClick() {alert("I got clicked!");}
