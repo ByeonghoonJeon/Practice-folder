c3861b34b26e1192016c0afb6500d3d46b961507
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-20 00:17:35 +0700

    Practice

--- javascript practice/Drum Kit Challenge/index.js
+++ javascript practice/Drum Kit Challenge/index.js
@@ -1,3 +1,9 @@
 for (var n = 0; n<7; n++){
-document.querySelectorAll("button")[n].addEventListener("click",handleClick);}
-function handleClick() {alert("I got clicked!");}
+document.querySelectorAll("button")[n].addEventListener("click",audio);}
+
+var audio = new Audio("sounds/tom-1.mp3");
+
+
+function audio(){audio.play()}
+
+
