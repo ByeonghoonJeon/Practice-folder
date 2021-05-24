87b6fcab197a6bdfb04674e8a3233614b926592d
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-24 15:23:06 +0700

    Practice

--- javascript practice/Drum Kit Challenge/index.js
+++ javascript practice/Drum Kit Challenge/index.js
@@ -1,3 +1,6 @@
+
+
+
 var audio = new Audio("sounds/tom-1.mp3");
 audio.play();
 for (var n = 0; n<7; n++){
@@ -5,6 +8,6 @@ document.querySelectorAll("button")[n].addEventListener("click",function(){
 var audio = new Audio("sounds/tom-1.mp3");
 audio.play();
 })}
-
-
+document.querySelector("button").addEventListener("click", handleClick);
+function handleClick(){alert ("I got clicked!");}
 
