012d88114a304a74be72925f3bdab053f4b3ffe5
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-24 12:57:57 +0700

    Practice

--- javascript practice/phraseLengthChecker.js
+++ javascript practice/phraseLengthChecker.js
@@ -1,4 +1,7 @@
-var writingLimit = 140
-var currentLength = prompt("Write your opinion.").length
-var availableLength = writingLimit - currentLength
-alert ("You have "+availableLength+" letters more.")
+var writingLimit = 140;
+var opinion = prompt("Write your opinion.");
+var currentLength = opinion.length;
+var availableLength = writingLimit - currentLength;
+alert ("You have written "+currentLength+" letters and you have "+availableLength+" letters more.");
+var under140 = opinion.slice(0,140);
+if (currentLength>=140){alert ("Your opinion exceeds 140 letters. It will be shown as following: "+under140)};
