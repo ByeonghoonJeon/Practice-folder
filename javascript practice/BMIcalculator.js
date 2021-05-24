302675068ad3919508e16f1ff068e93dd2c12d61
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-24 13:25:32 +0700

    Practice

---
+++ javascript practice/BMIcalculator.js
@@ -0,0 +1,4 @@
+var weight = prompt ("Please input your body weight (kg): ");
+var height = prompt ("Please input your height (m): ");
+var bmi = weight / (height * height);
+alert ("Your BMI: "+bmi);
