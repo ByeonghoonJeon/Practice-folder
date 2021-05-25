1d2340e44a3a738169d2fe1e5f1480daf1913258
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-25 19:43:58 +0700

    Practice

--- javascript practice/jQuery practice/index.js
+++ javascript practice/jQuery practice/index.js
@@ -1,4 +1,16 @@
 $("h1").css("color", "red");
 
 $("h1").addClass("center_title");
-$("button").html("<em>Click it</em>")
+$("button").html("<em>Click it</em>");
+
+for (var n = 0; n < 5; n++) {
+  document.querySelectorAll("button")[n].addEventListener("click", function () {
+    document.querySelector("h1").style.color = "blue";
+  });
+}
+
+for (var i = 0; i < 5; i++) {
+  $("button")[i].addEventListener("click", function () {
+    $("h1").css("color", "white");
+  });
+}
