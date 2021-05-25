cf0bf8ce5171b0f964998ee10e3bd1eaa7955c6c
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-25 14:42:23 +0700

    Practice

--- javascript practice/Drum Kit Challenge/index.js
+++ javascript practice/Drum Kit Challenge/index.js
@@ -1,11 +1,44 @@
-var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
-for (var n = 0; n<7; n++){
-document.querySelectorAll("button")[n].addEventListener("click",function(){
-var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
-})}
+for (var n = 0; n < 7; n++) {
+  document.querySelectorAll("button")[n].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
 
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var kickBase = new Audio("sounds/kick-bass.mp3");
+        kickBase.play();
+        break;
+      case "l":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      default:
+        break; console.log();
+    }
+
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+  });
+}
 
 //querySelector("class>children") is for direct children of class
 //uerySelector("class .children") is for all the element decendant of class(including not direct)
