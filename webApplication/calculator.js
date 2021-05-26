ce68cbb1059cd6f7a53505dc823122804a9355ef
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-26 20:39:47 +0700

    Practice

--- webApplication/calculator.js
+++ webApplication/calculator.js
@@ -1,39 +1,33 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-  var num1 = Number(req.body.n1);
-  var num2 = Number(req.body.n2);
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
   var result = num1 + num2;
 
   res.send("The result of the calculation is " + result);
 });
 
-app.get("/BMIcalculator", function(req,res){
-    res.sendFile(__dirname+"/BMIcalculator.html")
+app.get("/BMIcalculator", function (req, res) {
+  res.sendFile(__dirname + "/BMIcalculator.html");
 });
 
-app.post("/BMIcalculator", function(req, res){
-    var weight = parseFloat(req.body.weight);
-    var height = parseFloat(req.body.height);
+app.post("/BMIcalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
 
-    var bmi = weight/(height*height);
+  var bmi = weight / (height * height);
 
-    res.send("Your BMI is " + bmi);
+  res.send("Your BMI is " + bmi);
 });
-
-
-
 
 app.listen(3000, function () {
   console.log("Server started on the port 3000");
