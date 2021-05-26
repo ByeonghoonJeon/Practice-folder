72fd68b8b8fbf198bd02cf8d57d5b60f010ac471
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-26 09:51:48 +0700

    Practice

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,2 +1,3 @@
-const fs = require("fs");
-fs.copyFileSync("file1.txt", "file2.txt");
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
