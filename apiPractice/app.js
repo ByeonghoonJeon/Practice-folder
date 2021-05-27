65b69a11384536e32abbe1a308c17806515bbd32
ByeonghoonJeon <81186388+ByeonghoonJeon@users.noreply.github.com>
2021-05-27 20:57:14 +0700

    Practice

--- apiPractice/app.js
+++ apiPractice/app.js
@@ -13,9 +13,17 @@ app.get("/", function(req, res){
             const temperature = weatherData.main.temp;
             const temp = Math.floor(temperature);
             console.log(temp);
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            console.log(weatherDescription);
+            res.write("<h1>The weather is currently "+ weatherDescription+"</h1>");
+            res.write("<h1>The temperature in Vienna is "+temp+" degree celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send()
         })
     });
-    res.send("Server is up and running.");
+
 })
 
 
