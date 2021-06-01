273f59b5d2a16ccf6a93219688ca6cb6d10d34a5
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-01 20:56:56 +0700

    Practice

--- todoList-v1.0/app.js
+++ todoList-v1.0/app.js
@@ -2,10 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+
+//when a user approaches to the home = app.get("/", fu...)
 app.get("/", function(req, res){
-    res.send("Hello");
+    var today = new Date();
+    if (today.getDay()=== 6 || today.getDay()===0){
+        res.send("Yay it's the weekend!");
+    }
+    else{
+        res.send("Boo! I have to work");
+    }
+//getDay = gives number values according to the day (Sun =0 Mon=1 Tue =2...)
 });
 
-app.listen(1992, function(){
+
+//show screen above.
+
+
+// and listen via port xxxx 
+app.listen(1992, function(){ 
     console.log("Server started on port 1992");
 });
