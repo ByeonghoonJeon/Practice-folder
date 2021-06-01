a5f21bab5702e93fa89f21cdabf7a17c83d55e50
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-01 22:54:33 +0700

    Practice

--- todoList-v1.0/app.js
+++ todoList-v1.0/app.js
@@ -10,15 +10,47 @@ app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
     var day = "";
+    
+    alert(currentDay);
 
-    if (currentDay === 6 || currentDay=== 0){
-        day = "weekend";
+    if (currentDay === 0){
+        day = "Sunday";
         res.render("list", {
             kindOfDay: day
         });
     }
-    else{
-        day = "weekday";
+    else if(currentDay===1){
+        day = "Monday";
+        res.render("list", {
+            kindOfDay: day
+        });
+    }
+    else if(currentDay===2){
+        day = "Tuesday";
+        res.render("list", {
+            kindOfDay: day
+        });
+    }
+    else if(currentDay===3){
+        day = "Wednesday";
+        res.render("list", {
+            kindOfDay: day
+        });
+    }
+    else if(currentDay===4){
+        day = "Thursday";
+        res.render("list", {
+            kindOfDay: day
+        });
+    }
+    else if(currentDay===5){
+        day = "Friday";
+        res.render("list", {
+            kindOfDay: day
+        });
+    }
+    else if(currentDay===6){
+        day = "Saturday";
         res.render("list", {
             kindOfDay: day
         });
@@ -31,6 +63,6 @@ app.get("/", function(req, res){
 
 
 // and listen via port xxxx 
-app.listen(1992, function(){ 
+app.listen(3000, function(){ 
     console.log("Server started on port 1992");
 });
