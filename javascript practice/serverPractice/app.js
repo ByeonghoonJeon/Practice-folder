2448506858a12469ecb6e88ab26d9cb04af99311
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-04 17:02:24 +0700

    Practice

--- javascript practice/serverPractice/app.js
+++ javascript practice/serverPractice/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+
 // const path = require('path');
 
 app.use(express.static('public'));
 // app.get("/", function(req,res){
 //     res.sendFile(__dirname+"/index.html");
 // });
+
+
+
 
 app.listen(3000, function(){
     console.log("Server, port 3000");
