e4b4e333f14f8cd91ccae33f162cef0f07172bfd
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-19 11:48:58 +0700

    Practice

--- javascript practice/Dicee Challenge/index.js
+++ javascript practice/Dicee Challenge/index.js
@@ -1,35 +1,39 @@
-const ranFloat = Math.random()*6;
-const randomNumber1 = Math.floor(ranFloat)+1;
-let imgSource = document.querySelector(".img1").src;
+const ranFloat1 = Math.random()*6;
+const randomNumber1 = Math.floor(ranFloat1)+1;
+let imgSource1 = "";
+
+const ranFloat2 = Math.random()*6;
+const randomNumber2 = Math.floor(ranFloat2)+1;
+let imgSource2 = "";
 
 switch (randomNumber1) {
     case 1:
-        imgSource = "images/dice1.png";
+        imgSource1 = "images/dice1.png";
         
         break;
 
     case 2:
-        imgSource = "images/dice2.png";
+        imgSource1 = "images/dice2.png";
         
         break;
 
     case 3:
-        imgSource = "images/dice3.png";
+        imgSource1 = "images/dice3.png";
         
         break;
     
     case 4:
-        imgSource = "images/dice4.png";
+        imgSource1 = "images/dice4.png";
         
         break;
 
     case 5:
-        imgSource = "images/dice5.png";
+        imgSource1 = "images/dice5.png";
         
         break;
 
     case 6:
-        imgSource = "images/dice6.png";
+        imgSource1 = "images/dice6.png";
         
         break;  
     
@@ -37,5 +41,42 @@ switch (randomNumber1) {
         console.log("The number is not 1");
         break;
 };
-document.body.querySelector(".img1").setAttribute("src", imgSource)
-console.log(imgSource);
+
+
+switch (randomNumber2) {
+    case 1:
+        imgSource2 = "images/dice1.png";
+        
+        break;
+
+    case 2:
+        imgSource2 = "images/dice2.png";
+        
+        break;
+
+    case 3:
+        imgSource2 = "images/dice3.png";
+        
+        break;
+    
+    case 4:
+        imgSource2 = "images/dice4.png";
+        
+        break;
+
+    case 5:
+        imgSource2 = "images/dice5.png";
+        
+        break;
+
+    case 6:
+        imgSource2 = "images/dice6.png";
+        
+        break;  
+    
+    default:
+        console.log("The number is not 1");
+        break;
+};
+document.body.querySelector(".img1").setAttribute("src", imgSource1);
+document.body.querySelector(".img2").setAttribute("src", imgSource2);
