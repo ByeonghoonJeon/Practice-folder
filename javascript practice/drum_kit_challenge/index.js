2c161ee3c6b77f7effeb1abc739d6ee8080c812a
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-22 00:24:18 +0700

    Practice

--- javascript practice/drum_kit_challenge/index.js
+++ javascript practice/drum_kit_challenge/index.js
@@ -1,16 +1,26 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
     
+    var buttonInnerHTML = this.innerHTML;
+    
+        switch (buttonInnerHTML) {
+            case "w": 
+            let audio = new Audio ("sounds/tom-1.mp3");
+            audio.play();
+                break;
+        
+            default:
+                break;
+    }
+    
+    
+ 
+        this.style.color="white";
+    });
 }
 
-let audio = new Audio ("sounds/tom-1.mp3");
 
 
-
-function handleClick(){
-    audio.play();
-    this.style.color="white";
-}
 
