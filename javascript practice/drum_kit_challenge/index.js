86f42772ea10b0cbd40306529386212f95e20d08
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-22 14:14:36 +0700

    Practice

--- javascript practice/drum_kit_challenge/index.js
+++ javascript practice/drum_kit_challenge/index.js
@@ -49,6 +49,58 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   });
 }
 
+document.addEventListener("keydown", function(event){
+    switch (event.key) {
+        case "w":
+          let crashAudio= new Audio("sounds/crash.mp3");
+          crashAudio.play();
+          document.querySelector(".w",".drum").style.color="white";
+          break;
+          
+  
+        case "a":
+          let kickBassAudio = new Audio("sounds/kick-bass.mp3");
+          kickBassAudio.play();
+          document.querySelector(".a", ".drum").style.color="white";
+          break;
+  
+        case "s":
+            let snareAudio = new Audio("sounds/snare.mp3");
+            snareAudio.play();
+            document.querySelector(".s", ".drum").style.color="white";
+            break;
+      
+        case "d":
+            let tom1Audio = new Audio("sounds/tom-1.mp3");
+            tom1Audio.play();
+            document.querySelector(".d", ".drum").style.color="white";
+            break;
+          
+        case "j":
+            let tom2Audio = new Audio("sounds/tom-2.mp3");
+            tom2Audio.play();
+            document.querySelector(".j", ".drum").style.color="white";
+            break;
+          
+        case "k":
+            let tom3Audio = new Audio ("sounds/tom-3.mp3");
+            tom3Audio.play();
+            document.querySelector(".k", ".drum").style.color="white";
+            break;
+  
+        case "l":
+            let tom4Audio = new Audio ("sounds/tom-4.mp3");
+            tom4Audio.play();
+            document.querySelector(".l", ".drum").style.color="white";
+            break;
+          
+        default:
+          break;
+      }
+      
+});
+
+
 
 function workerDatabase(name, gender, age, department){
     this.name = name;
