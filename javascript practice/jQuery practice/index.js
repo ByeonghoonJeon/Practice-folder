5d3305d80facaaa72dd590e7cd94f61962b3c232
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-22 21:25:23 +0700

    Practice

--- javascript practice/jQuery practice/index.js
+++ javascript practice/jQuery practice/index.js
@@ -1,16 +1,5 @@
-$("h1").css("color", "red");
-
-$("h1").addClass("center_title");
-$("button").html("<em>Click it</em>");
-
-for (var n = 0; n < 5; n++) {
-  document.querySelectorAll("button")[n].addEventListener("click", function () {
-    document.querySelector("h1").style.color = "blue";
-  });
-}
-
-for (var i = 0; i < 5; i++) {
-  $("button")[i].addEventListener("click", function () {
-    $("h1").css("color", "white");
-  });
-}
+$(".textBox").keydown(function(event){
+  let typedText = event.key;
+   $("h1").text(typedText);
+  console.log(typedText)
+});
