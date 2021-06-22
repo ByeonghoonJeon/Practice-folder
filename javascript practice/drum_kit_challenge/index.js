03734ee17412dbecaed5bfb84730c250c2f54609
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-22 15:17:17 +0700

    Practice

--- javascript practice/drum_kit_challenge/index.js
+++ javascript practice/drum_kit_challenge/index.js
@@ -57,53 +57,53 @@ document.addEventListener("keydown", function(event){
     switch (event.key) {
         case "w":
           let crashAudio= new Audio("sounds/crash.mp3");
-          crashAudio.play();
-          document.querySelector(".w",".drum").style.color="white";
+          crashAudio.play();          
           break;
           
   
         case "a":
           let kickBassAudio = new Audio("sounds/kick-bass.mp3");
-          kickBassAudio.play();
-          document.querySelector(".a", ".drum").style.color="white";
+          kickBassAudio.play();          
           break;
   
         case "s":
             let snareAudio = new Audio("sounds/snare.mp3");
-            snareAudio.play();
-            document.querySelector(".s", ".drum").style.color="white";
+            snareAudio.play();            
             break;
       
         case "d":
             let tom1Audio = new Audio("sounds/tom-1.mp3");
-            tom1Audio.play();
-            document.querySelector(".d", ".drum").style.color="white";
+            tom1Audio.play();            
             break;
           
         case "j":
             let tom2Audio = new Audio("sounds/tom-2.mp3");
-            tom2Audio.play();
-            document.querySelector(".j", ".drum").style.color="white";
+            tom2Audio.play();            
             break;
           
         case "k":
             let tom3Audio = new Audio ("sounds/tom-3.mp3");
-            tom3Audio.play();
-            document.querySelector(".k", ".drum").style.color="white";
+            tom3Audio.play();            
             break;
   
         case "l":
             let tom4Audio = new Audio ("sounds/tom-4.mp3");
-            tom4Audio.play();
-            document.querySelector(".l", ".drum").style.color="white";
+            tom4Audio.play();            
             break;
           
         default:
           break;
       }
+    
+      let pressedKey = document.querySelector(`.${event.key}`);
+      if (pressedKey.style.color === "white"){
+          pressedKey.style.color = "#DA0463";
+      }else{
+          pressedKey.style.color="white";
+      }
       
       
-});
+    });
 
 
 
