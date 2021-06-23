8dd1d36f32417f4a2ac2371e7a05a71ca195dcfb
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-23 16:56:13 +0700

    Practice

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,1 +1,17 @@
+const { response } = require("express");
 const express = require("express");
+const app = express();
+app.get("/", function(req, res){
+    res.send("Hello, world!");
+});
+
+app.get("/contact", function(req,res){
+    res.send("Contact page");
+});
+
+app.get("/about", function(req,res){
+    res.send("About me");
+});
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
