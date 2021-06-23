02dd714a1b3163ddb04ff0bdc45f4d8f7eb7d599
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-23 21:20:55 +0700

    Practice

--- javascript practice/BMIcalculator.js
+++
@@ -1,4 +0,0 @@
-var weight = prompt ("Please input your body weight (kg): ");
-var height = prompt ("Please input your height (cm): ");
-var bmi = weight / ((height/100) * (height/100));
-alert ("Your BMI: "+bmi);
--- javascript practice/webCalculator/calculator.js
+++ javascript practice/webCalculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
+
+app.post("/", function(req,res){
+
+var number1 = Number(req.body.num1);
+var number2 = Number(req.body.num2);
+var result = number1 + number2; 
+    res.send(`${result}`);
+});
+
 
 app.listen(3000, function(){
     console.log("Server is working");
