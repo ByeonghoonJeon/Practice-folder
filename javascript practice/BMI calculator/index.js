f95fb625d675d3fe61436ceaa2a40ce0d1a96d46
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-23 21:30:09 +0700

    Practice

--- javascript practice/BMI calculator/index.js
+++ javascript practice/BMI calculator/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req,res){
+    let weight = Number(req.body.bodyWeight);
+    let cmHeight = Number(req.body.height);
+    let height = cmHeight/100;
+    let bmiIndex = Math.floor(weight/Math.sqrt(height));
+    res.send("Your BMI index is " + bmiIndex)
 })
-
-
 
 
 app.listen(3000, function(){
