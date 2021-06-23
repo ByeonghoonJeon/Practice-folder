4f597828b839dd1688adbfc1e805808915dfdfac
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-23 14:33:53 +0700

    Practice

--- intro-to-node/index.js
+++
@@ -1,7 +0,0 @@
-var superheroes = require("superheroes");
-var mySuperheroName = superheroes.random();
-console.log(mySuperheroName);
-
-var supervillains = require("supervillains");
-var mySupervillainsName=supervillains.random();
-console.log(mySupervillainsName);
--- nodePractice/module1.js
+++
@@ -1,7 +0,0 @@
-function myFunction(){
-    console.log("Function is called");
-    }
-
-var myString = "String!";
-module.exports.myFunction = myFunction; //When function itself is not required to be executed, paranthesis is not included.
-module.exports.myString = myString;
