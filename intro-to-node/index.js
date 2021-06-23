43153b9488c77367f0e43af722aaac05d8be1f1b
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-23 15:26:46 +0700

    Practice

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,4 +1,9 @@
 
-console.log("Hello, world!");
-const fs = require("fs");
 const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+console.log("Hello, world!");
+
+let mySuperhero = superheroes.random();
+let mySupervillain = supervillains.random();
+console.log(mySuperhero);
+console.log(mySupervillain);
