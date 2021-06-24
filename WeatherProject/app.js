e0d4e1ed5e0671cb02e39f9481b6d87010b19d1d
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-24 19:44:32 +0700

    Practice

---
+++ WeatherProject/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { Http2ServerResponse } = require("http2");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req,res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Vienna&appid=51fad77e11441b96834a30e61a4d1f50&units=metric";
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            console.log(weatherDescription);
+            
+        });
+    });
+    res.send("weatherDescription")
+})
+
+app.listen(3000, function(){
+    console.log("Server is working");
+});
