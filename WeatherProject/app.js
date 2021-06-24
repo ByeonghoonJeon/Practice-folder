ce8d22d5ad4eace6a4afb9683dfe277df97cd96b
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-24 23:09:08 +0700

    Practice

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static('public'))
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
