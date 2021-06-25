4c2f259c21d565b1a827f2565d8ddf91f8f10684
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-25 23:25:40 +0700

    Practice

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const app = express();
+const https = require("https");
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
@@ -13,6 +14,38 @@ app.post("/", function(req,res){
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData=JSON.stringify(data);
+    const url = "https://us6.api.mailchimp.com/3.0/lists/ea756b17bc";
+    const options = {
+        method: "POST",
+        auth: s// "Name:API key
+    }
+    
+
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+    
+    
     res.write(firstName);
     res.write(lastName);
     res.write(email);
@@ -23,3 +56,7 @@ app.post("/", function(req,res){
 app.listen(3000, function(){
     console.log("Server is running.");
 });
+
+//ea756b17bc
+
+
