04b90920e600301e838af3163f6702867acb9b21
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-25 21:52:00 +0700

    Practice

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const request = require("request");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/", function(req,res){
-    res.send("Hello World!");
+    res.sendFile(__dirname+"/signup.html");
 });
 
 app.listen(3000, function(){
