b547cf8c9626b537823f47236124694ba3240142
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-28 19:37:23 +0700

    Practice

--- todoList_v1/app.js
+++ todoList_v1/app.js
@@ -1,21 +1,46 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine', "ejs");
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", function(req,res){
-    var today = new Date();
-    var currentDay = today.getDay();
-    if (currentDay===6 || currentDay===0){
-        res.sendFile(__dirname+ "/weekdays.html");
-    }else{
-        res.sendFile(__dirname+ "/weekend.html");
-    }
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+      break;
+  }
+
+  res.render("list", { kindOfDay: day });
 });
 
-app.listen(3000, function(){
-    console.log("Server is running.")
-})
+app.listen(3000, function () {
+  console.log("Server is running.");
+});
