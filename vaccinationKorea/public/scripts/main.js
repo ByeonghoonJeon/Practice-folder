e1d22dc872f8e19dc598553af28b6e315f5c5412
Nathan Jeon <jeon4972@snu.ac.kr>
2021-06-29 15:30:10 +0700

    Practice

--- vaccinationKorea/public/scripts/main.js
+++ vaccinationKorea/public/scripts/main.js
@@ -1,7 +1,25 @@
-// getDate = function () {
-//     const today = new Date();
-//     const options = { weekday: "long", day: "numeric", month: "long" };
-//     return today.toLocaleDateString("en-US", options);
-//   };
+function getDate () {
+    const today = new Date();
+    const options = { weekday: "long", day: "numeric", month: "long" };
+    return today.toLocaleDateString("en-US", options);
+  };
 
-// console.log
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+
+if (day < 10){
+  day = "0"+day;
+}
+
+if (month <10){
+  month = "0"+month;
+}
+
+let fullDate = `${year}-${month}-${day}`;
+
+console.log(fullDate);
+
+document.querySelector(".date").textContent = getDate();
+document.querySelector("#queryDate").setAttribute("value",fullDate);
