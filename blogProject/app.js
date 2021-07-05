5c3de347481c1d554b6d98bc2dfb3be8c62d0c00
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-05 17:01:55 +0700

    Practice

--- blogProject/app.js
+++ blogProject/app.js
@@ -12,8 +12,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+  res.render("home",{homeStartingContent:homeStartingContent});
+});
 
 
 
