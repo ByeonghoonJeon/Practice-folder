5a0dbe00601062bf0f7ea03eb13a655f88658966
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-06 10:53:57 +0700

    Practice

--- blogProject/app.js
+++ blogProject/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { request } = require("express");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -23,7 +24,7 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("home", {
     homeStartingContent: homeStartingContent,
-    posts:posts
+    posts: posts,
   });
 });
 
@@ -40,11 +41,24 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  let title = req.body.title;
-  let post = req.body.post;
-  let postContent = { title: title, post: post };
-  posts.push(postContent);
+  const post = {
+    title: req.body.title,
+    content: req.body.post,
+  };
+
+  posts.push(post);
   res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
+  posts.forEach(function (post) {
+    const storedTitle = post.Title;
+  });
+
+  if (requestedTitle == storedTitle) {
+    console.log("It Matches!");
+  }
 });
 
 app.listen(3000, function () {
