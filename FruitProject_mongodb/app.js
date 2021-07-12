737310a1cc6f488b70cd3594d85162cd18aee4f1
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-12 10:37:22 +0700

    Practice

--- FruitProject_mongodb/app.js
+++ FruitProject_mongodb/app.js
@@ -9,15 +9,28 @@ const url = "mongodb://localhost:27017";
 // Use connect method to connect to the Server
 MongoClient.connect(url, { useUnifiedTopology: true }, async function (err, client) {
   assert.equal(null, err);
-  const db = client.db("test");
+  const db = client.db("fruits");
 
-  await db.collection("inventory")
-    .insertOne({
-      item: "canvas",
-      qty: 100,
-      tags: ["cotton"],
-      size: { h: 28, w: 35.5, uom: "cm" },
-    })
+  await db.collection("fruits")
+    .insertMany([
+    {
+      name: "Apple",
+      score: 8,
+      revies: "Great fruit"
+    },
+    {
+        name: "Orange",
+        score: 6,
+        review: "Kinda sour"
+    },
+    {
+        name: "Banana",
+        score: 9,
+        review: "Great stuff!"
+
+    }
+
+    ])
 
   console.log("Working");
   client.close();
