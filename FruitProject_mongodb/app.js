3839d71c34795139ca2646177bd8a3fc9d7361c6
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-12 12:41:57 +0700

    Practice

--- FruitProject_mongodb/app.js
+++ FruitProject_mongodb/app.js
@@ -10,54 +10,69 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {useNewUrlParser: true, u
 
 
 //mongoose needs schema which is a blue print of data structure.
-// const fruitSchema = new mongoose.Schema ({
-//     _id: String,
-//     name: String,
-//     score: Number,
-//     review: String
-// });
-
-
-//collection creation, insert with singular form and it will be converted to plural form.
-const Fruit = mongoose.model('Fruit', {
+const fruitSchema = new mongoose.Schema ({
     _id: String,
     name: String,
     score: Number,
     review: String
 });
 
+
+//collection creation, insert with singular form and it will be converted to plural form.
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
 //Creating a document from the model.
 const fruit = new Fruit(
     {
-    _id: ObjectID(), 
-    name:"Peach",
-    score: 10,
-    review: "It's a perfect fruits!"
+        _id: ObjectID(), 
+        name:"Peach",
+        score: 10,
+        review: "It's a perfect fruits!"
     },
     {
-    _id: ObjectID(), 
-    name: 'Banana',
-    score: 8,
-    review: "Awesome fruit!"
+        _id: ObjectID(), 
+        name: 'Banana',
+        score: 8,
+        review: "Awesome fruit!"
     },
     {   
-    _id: ObjectID(), 
-    name: 'Apple',
-    score: 4,
-    review: "Not really.."
+        _id: ObjectID(), 
+        name: 'Apple',
+        score: 4,
+        review: "Not really.."
     }
 );
-fruit.save();
 
-// const personSchema = new mongoose.Schema({
-//     name: String,
-//     age: Number
-// });
+Fruit.insertMany([fruit], function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully saved all the data.")
+    }
+});
 
-// const Person = mongoose.model("Person", personSchema);
-// const person = new Person({
-//     name: "Nathan",
-//     age: 19
-// });
-// person.save();
+/////////////////////////////////////////////////////
+
+
+const personSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+const person = new Person(
+    {
+        _id: ObjectID(),
+        name: "Nathan",
+        age: 19
+    },
+    {
+        _id: ObjectID(),
+        name: "Edwin",
+        age: 18
+    }
+);
+person.save();
+
 console.log("Working!")
