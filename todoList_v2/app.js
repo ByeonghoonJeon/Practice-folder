1acc55178095cb86b4b1acde74258544e398187a
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-13 19:05:14 +0700

    Practice

--- todoList_v2/app.js
+++ todoList_v2/app.js
@@ -33,6 +33,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", function (req, res) {
   Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
@@ -48,6 +55,15 @@ app.get("/", function (req, res) {
       res.render("list", { listTitle: "Today", newListItems: foundItems });
     }
   });
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName=req.params.customListName;
+  const list = new List({
+    name: customListName,
+    items: defaultItems
+  });
+    list.save();
 });
 
 app.post("/", function (req, res) {
@@ -72,9 +88,7 @@ app.post("/delete", function(req, res){
   });
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
+
 
 app.get("/about", function (req, res) {
   res.render("about");
