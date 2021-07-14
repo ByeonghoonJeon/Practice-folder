8df840f81cc7eebc5a7e73a1001869c9963f6e29
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-14 23:25:27 +0700

    Practice

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -6,10 +6,17 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
-
 app.set('view engine', 'ejs');
-
 app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+const articleSchema = {
+  title: String,
+  contents: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 //TODO
 
