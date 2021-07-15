b8e71c872844546bf9557c86b47735fcad63a7e8
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-15 23:18:24 +0700

    Practice

--- cookie_session/app.js
+++ cookie_session/app.js
@@ -4,16 +4,11 @@ const ejs = require("ejs");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
-const saltRounds = 10;
-
-console.log(process.env.API_KEY);
 
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
@@ -38,41 +33,11 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
+  
 });
 
 app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, hash, function (err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
-          // result == true
-        });
-      }
-    }
-  });
+  
 });
 
 app.listen(3000, function () {
