bbcbd86acac6d60a0e563d036ae1bf0aa2b93b7e
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-15 17:04:06 +0700

    Practice

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
@@ -13,10 +14,13 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "Thisismysecret.";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
