296a68582edb97dc9512e5241acd5f4fba1ad572
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-15 23:29:37 +0700

    Practice

--- cookie_session/app.js
+++ cookie_session/app.js
@@ -4,12 +4,24 @@ const ejs = require("ejs");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const session = require("express-session");
+const passport = require("passport");
+const poassportLocalMongoose = require("passport-local-mongoose");
 
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -18,7 +30,13 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+userSchema.plugin(poassportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home");
