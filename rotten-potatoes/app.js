6ad726cde2275bc408e0be634f574e8dc99fcf8f
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-19 21:20:26 +0700

    Practice

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -3,12 +3,22 @@ const app = express();
 
 const exphbs = require("express-handlebars");
 
+let reviews = [
+    {title: "Great Review", movieTitle: "Batman II"},
+    {title: "Awesome Movie", movieTitle: "Titanic"}
+];
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get ("/", function(req, res){
-    res.render("home", {msg:"Handlebars are cool"});
+    res.render("reviews-index", {reviews:reviews});
 });
+
+app.get("/reviews", function(req, res){
+    res.render("reviews-index", {reviews: reviews});
+});
+
 
 app.listen(3000, function(){
     console.log("Server is working on port 3000");
