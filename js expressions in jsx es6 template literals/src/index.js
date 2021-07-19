3b0e0e1c6d300ea803793ed068d63f9c1c0f9e77
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-19 20:56:52 +0700

    Practice

---
+++ js expressions in jsx es6 template literals/src/index.js
@@ -0,0 +1,18 @@
+const React = require("react");
+const ReactDOM = require("react-dom")
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My List</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
